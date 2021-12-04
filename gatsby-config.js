f74956f63d96f28b0db0a2c0a11eeeb32f42873e
Tomasz Klepacki <tomaszklepacki@op.pl>
2021-12-04 22:07:38 +0100

    Add new files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/Domino_Website",
   siteMetadata: {
     title: `Waxon Portfolio React Template`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
